b45022805543df4cf72ba2cbde78fccc0bdee10b
deepakr <deepak.r1618@gmail.com>
2020-02-03 21:36:58 +0530

    test 1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn sample:app
+web: python flask_app.py
--- flask_app.py
+++ flask_app.py
@@ -51,4 +51,5 @@ def scrapeAndStore(usn , dob):
 
 
 if __name__ == '__main__':
-   app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port = port)
